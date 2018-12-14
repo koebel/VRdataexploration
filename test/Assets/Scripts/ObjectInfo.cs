58edabcac4ccb7916564b9069546032a43c9b747
Kathrin Koebel <kathrin.koebel@fhnw.ch>
2018-12-14 15:22:35 +0100

    Worldmap test

---
+++ test/Assets/Scripts/ObjectInfo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectInfo : MonoBehaviour {
+    public string object_ref;
+    public int object_id;
+    public string title;
+    public string artist;
+    public string geography;
+    public string continent;
+    public float coord_lat;
+    public float coord_long;
+    public string time;
+    public int time_from;
+    public int time_to;
+    public string material;
+    public string size;
+    public string origin;
+    public string provenance1;
+    public string provenance2;
+    public string provenance3;
+    public string provenance4;
+    public string provenance5;
+    public string description;
+}
