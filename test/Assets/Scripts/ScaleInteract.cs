e34f7131e4f8021ab29b3d933aa04db72a9e22ab
Kathrin Koebel <kathrin.koebel@fhnw.ch>
2019-01-05 15:05:51 +0100

    Scale Interact updated to show marker plus give visual feedback by change of color if selection is valid

--- test/Assets/Scripts/ScaleInteract.cs
+++ test/Assets/Scripts/ScaleInteract.cs
@@ -12,15 +12,29 @@ public class ScaleInteract : MonoBehaviour {
     // Timeline Interactor
     public GameObject controller;
     public GameObject camrig;
+    public GameObject zoomInteractor;
     public GameObject cube;
+
+    // child objects
+    private GameObject top;
+    private GameObject bottom;
+    private GameObject plus;
+    private GameObject minus;
 
     //Scale Factor
     public float scaleFactor = 2;
     private float currentScaleFactor;
-
     private bool scalingActive = true;
-    private bool touched = false;
 
+    // materials for main menu
+    public Material standardMaterial;
+    public Material selectedMaterial;
+
+    //Marker
+    private GameObject marker;
+    public float markerSize = 0.05f;
+    private float markerRadiusFactor = 0.265f;
+    private bool touched = false;
 
     // y_move measures the movement on the trackpad
     // values range between -1 and 1
@@ -33,10 +47,35 @@ public class ScaleInteract : MonoBehaviour {
     private float camrigY;
     private float camrigZ;
 
-
-
     void Start() {
-        currentScaleFactor = 1f;
+        currentScaleFactor = 1f * scaleFactor * scaleFactor;
+
+        // find child objects of zoom interactor 
+        top = zoomInteractor.transform.Find("zoom-top").gameObject;
+        top = top.transform.Find("default").gameObject;
+
+        bottom = zoomInteractor.transform.Find("zoom-bottom").gameObject;
+        bottom = bottom.transform.Find("default").gameObject;
+
+        plus = zoomInteractor.transform.Find("zoom-plus").gameObject;
+        plus = plus.transform.Find("default").gameObject;
+
+        minus = zoomInteractor.transform.Find("zoom-minus").gameObject;
+        minus = minus.transform.Find("default").gameObject;
+
+        top.GetComponent<MeshRenderer>().material = standardMaterial;
+        bottom.GetComponent<MeshRenderer>().material = selectedMaterial;
+
+        // create Marker that indicates input position on filter
+        marker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        Collider c = marker.GetComponent<Collider>();
+        c.enabled = false;
+        marker.SetActive(false);
+
+        // set position of marker
+        marker.transform.parent = zoomInteractor.transform;
+        marker.transform.localScale = new Vector3(markerSize, markerSize, markerSize);
+        marker.transform.localPosition = new Vector3(-0.275f, 0.1f, 0.275f);
     }
 
 
@@ -83,6 +122,54 @@ public class ScaleInteract : MonoBehaviour {
         }
         */
 
+        
+        // set position of pointer on filter menu on Touchpad Touch
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+
+            if (!touched)
+            {
+                // set position before activating marker
+                marker.transform.localPosition = new Vector3(device.GetAxis().x * markerRadiusFactor * (-1) - 0.275f, 0.1f, (device.GetAxis().y * markerRadiusFactor * (-1) + 0.275f));
+                marker.SetActive(true);
+                touched = true;
+            }
+            else
+            {
+                marker.transform.localPosition = new Vector3(device.GetAxis().x * markerRadiusFactor * (-1) - 0.275f, 0.1f, (device.GetAxis().y * markerRadiusFactor * (-1) + 0.275f));
+            }
+
+            // set color of buttons when active
+            // plus
+            if (device.GetAxis().y > 0)
+            {
+                bottom.GetComponent<MeshRenderer>().material = standardMaterial;
+
+                if (currentScaleFactor < 1025)
+                {
+                    top.GetComponent<MeshRenderer>().material = selectedMaterial;
+                }
+                else {
+                    top.GetComponent<MeshRenderer>().material = standardMaterial;
+                }
+            }
+            // minus
+            else if (device.GetAxis().y < 0)
+            {
+                top.GetComponent<MeshRenderer>().material = standardMaterial;
+
+                if (currentScaleFactor > 1)
+                {
+                    bottom.GetComponent<MeshRenderer>().material = selectedMaterial;
+                }
+                else
+                {
+                    bottom.GetComponent<MeshRenderer>().material = standardMaterial;
+                }
+            }
+        }
+        
+
         // Checks for Touchpad clicks for stepwise scaling      
         if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
         {
@@ -110,13 +197,6 @@ public class ScaleInteract : MonoBehaviour {
 
                 Transform transform = cube.transform;
                 camrig.transform.localScale = new Vector3(currentScaleFactor, currentScaleFactor, currentScaleFactor);
-
-                // adjust cam hight
-                /*
-                if (camrigY < 1) {
-                    camrig.transform.localPosition = new Vector3(camrigX, 1.0f, camrigZ);
-                }
-                */
             }
         }
 
@@ -124,6 +204,11 @@ public class ScaleInteract : MonoBehaviour {
         if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
         {
             y_move = 0.0f;
+
+            // hide marke on touch up
+            marker.SetActive(false);
+            top.GetComponent<MeshRenderer>().material = standardMaterial;
+            bottom.GetComponent<MeshRenderer>().material = standardMaterial;
             touched = false;
         }   
     }
