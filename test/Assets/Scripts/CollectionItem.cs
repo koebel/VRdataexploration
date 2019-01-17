e31b43fdde416617df59b8ae686a120b597190f6
Kathrin Koebel <kathrin.koebel@fhnw.ch>
2019-01-17 15:31:44 +0100

    set material of collection item dynamically

--- test/Assets/Scripts/CollectionItem.cs
+++ test/Assets/Scripts/CollectionItem.cs
@@ -13,14 +13,16 @@ public class CollectionItem : MonoBehaviour {
     private GameObject ui;
     private GameObject background;
 
-    public Material pictureMaterial;
+    private Material pictureMaterial;
     public Material backgroundMaterial;
 
     private Color backgroundColor;
-    private float backgroundOpacity = 0.5f;
+    private float backgroundOpacity = 0.2f;
 
     private Text itemText;
     private string demoText = "Demo Text... Lorem Ipsum bla bla bla... ";
+    private string materialPath = "Objects/Materials/";
+    private string picturePath;
 
 
     // Use this for initialization
@@ -29,13 +31,14 @@ public class CollectionItem : MonoBehaviour {
         // define materials
         // backgroundMaterial = (Material)Resources.Load("Materials/Backstein", typeof(Material));
 
+        picturePath = string.Concat(materialPath, reference);
+        //pictureMaterial = (Material)Resources.Load("Objects/Materials/RAF721", typeof(Material));
+        pictureMaterial = (Material)Resources.Load(picturePath, typeof(Material));
+
         // set references to child objects
         picture = this.transform.Find("picture").gameObject;
         ui = this.transform.Find("UI").gameObject;
         background = this.transform.Find("background").gameObject;
-        Debug.Log("****************************************");
-        Debug.Log(ui.name);
-        Debug.Log("****************************************");
 
         // set materials
         // background material & opacity
@@ -49,8 +52,9 @@ public class CollectionItem : MonoBehaviour {
         picture.GetComponent<MeshRenderer>().material = pictureMaterial;
 
         // set text
-        //itemText = ui.GetComponentInChildren<Text>();
-        //itemText.GetComponent<Text>().supportRichText = true;
+        itemText = ui.GetComponentInChildren<Text>();
+        itemText.GetComponent<Text>().supportRichText = true;
+        itemText.text = demoText;
         //itemText.text = demoText.Replace("<br>", "\n");
     }
 	
